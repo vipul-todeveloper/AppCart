7f7def8e076e0b6f8896bd0fc182a18e5c888554
unknown <abc@(none)>
2014-09-15 11:59:17 +0530

    added login form demo

--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -2,10 +2,50 @@
 
 angular.module('appShopApp')
   .controller('MainCtrl', function ($scope, $http) {
-    $scope.awesomeThings = [];
+        var mathod = 'POST';
+        var inserturl = "http://192.168.0.17:9000/api/admin";
+        $scope.CodeStatus = "";
+        $scope.save = function() {
+            //http://192.168.0.17:3000/v0/admin?firstname=Mehul &user=me&pass=4889&email=me@gmail.com
+            var formData = {
+                'firstname' : this.fName,
+                'user' : this.uName,
+                'pass' : this.password,
+                'email' : this.email
+            }
 
-    $http.get('/api/things').success(function(awesomeThings) {
-      $scope.awesomeThings = awesomeThings;
-    });
+            this.fName = "";
+            this.uName = "";
+            this.password = "";
+            this.email = "";
+
+//        var jdata = 'mydata='+JSON.stringify(formData);
+
+            $http({
+                method: mathod,
+                url:inserturl,// 'http://192.168.0.17:3000/v0/admin?firstname=Mehul Mali&user=Mehul4889&pass=hi&email=mehul@gmail.com',
+                data: JSON.stringify(formData),
+//                        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+                cache: $templateCache
+            }).
+                success(function(response) {
+                    if(response.IsSuccess){
+                        console.log("success");
+
+                        alert(response.msg)
+                    }else{
+                        alert(response.msg)
+                        console.log(response.desc)
+                    }
+                    console.log("success");
+                    $scope.codeStatus = response.msg;
+                    console.log($scope.codeStatus);
+                }).
+                error(function(response) {
+                    console.log("error");
+                    alert("Error")
+
+                });
+        }
 
   });
